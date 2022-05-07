e0ff950f0300c1611d4d55a19d08644cc4aa3f63
Khartanovich <kick00000@mail.ru>
2022-05-07 21:10:07 +0300

    feat: add ESLint

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const wrapper = document.createElement("div");
+const title = document.createElement("h1");
+const textarea = document.createElement("textarea");
+const keyboardElem = document.createElement("div");
+const info = document.createElement("p");
+
+wrapper.classList.add("wrapper");
+title.textContent = "RSS Virtual Keyboard";
+info.innerText = "The virtual keyboard was created in Windows OS\nPush Ctrl + Alt for changing language";
+
+document.body.appendChild(wrapper);
+wrapper.appendChild(title);
+wrapper.appendChild(textarea);
+wrapper.appendChild(keyboardElem);
+wrapper.appendChild(info);
+
