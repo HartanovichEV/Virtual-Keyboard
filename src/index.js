4e67ded0ba41f40acdd91aeb2c84d99da793ad68
Khartanovich <kick00000@mail.ru>
2022-05-10 11:48:26 +0300

    fead: add language change

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ textarea.classList.add("use-keyboard-input");
 textarea.setAttribute("rows", "1");
 textarea.setAttribute("placeholder", "Well, click me and write something good....");
 title.textContent = "Virtual Keyboard";
-info.innerText = "The virtual keyboard was created in Windows OS\nPush Ctrl + Alt for try ";
+info.innerText = "The virtual keyboard was created in Windows OS\nPush Ctrl + Alt to change the language ";
 
 /* Textarea авто расширение*/
 textarea.addEventListener("keydown", autosize);
@@ -85,7 +85,7 @@ const Keyboard = {
             const codeLayout = [];
             (function () {
                 KEYS.forEach((key) => {
-                    if(languageEn) codeLayout.push(key.code);
+                    codeLayout.push(key.code);
                 });            
             }());
             const keyElement = document.createElement("button");
@@ -248,7 +248,10 @@ const Keyboard = {
                     
                     languageEn == true? languageEn = false: languageEn = true;
                     this.elements.main.innerHTML = "";
-                    this.init();
+                    this.elements.keysContainer = document.createElement("div");
+                    this.elements.keysContainer.classList.add("keyboard__keys");
+                    this.elements.keysContainer.appendChild(this._createKeys());
+                    this.elements.main.appendChild(this.elements.keysContainer);
                 }
                 else if (keyEvent.code === "Backspace") this.insertText("", "Backspace");
                 else if (keyEvent.code === "Delete") this.insertText("", "Delete");
